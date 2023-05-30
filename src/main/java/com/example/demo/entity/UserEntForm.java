211bffdef343a9b21b502c58e332486673807b15
JqRLCCYdVCkVkEJvmQkg <tkuramoto@stansystem.co.jp>
2023-05-30 10:20:40 +0900

    ログインシステム用の受け渡しのクラス2つとSQLファイル2つの計4つのファイルです。

---
+++ src/main/java/com/example/demo/entity/UserEntForm.java
@@ -0,0 +1,37 @@
+package com.example.demo.entity;
+
+public class UserEntForm {
+	
+	private int id;
+	private String logId;
+	private String pass;
+	
+	public UserEntForm() {
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getLogId() {
+		return logId;
+	}
+
+	public void setLogId(String logId) {
+		this.logId = logId;
+	}
+	
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+}
